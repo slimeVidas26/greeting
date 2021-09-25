5e5b2ea3be5d06b59fb02ba49d537c3ade706284
SlimeVidas <dahan.isaac@gmail.com>
2021-09-25 20:56:20 +0300

    querying with the query language

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # greeting
+
+* create git repo and clone to greeting
+*npm init -y
+* npm i graphql apollo-server
+* create server.js file
---
+++ server.js
@@ -0,0 +1,35 @@
+
+//DEPENDENCIES
+const {ApolloServer , gql} = require('apollo-server');
+
+
+//TYPEDEFS
+const typeDefs = gql`
+
+schema {
+query : Query
+}
+
+type Query {
+    greeting:String
+}
+`
+
+//console.log(typeDefs)
+
+
+//RESOLVERS
+const resolvers = {
+    Query :{
+        greeting : ()=> "Hello from Graphql"
+    }
+}
+
+
+const server = new ApolloServer({typeDefs , resolvers})
+
+
+server.listen({port : 9000})
+.then((serverInfo)=>{
+    console.log(serverInfo.url)
+})
